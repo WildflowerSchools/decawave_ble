584231d57eecebcc8fe1751ff6993d9cac1fc7f4
Paul DeCoursey <paul@decoursey.net>
2019-02-08 10:29:12 -0600

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='decawave_ble',
     packages=['decawave_ble'],
-    version='0.8.0',
+    version='0.9.0',
     include_package_data=True,
     description='Python toolset for working with and configuring the Decawave DWM1000 devices',
     long_description=open('README.md').read(),
